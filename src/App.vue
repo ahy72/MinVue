5121ba4e89cd8d49218c6c2cc87bf2df8b11318b
大野 洋平 <y-ohno@ad.pca.co.jp>
2020-03-24 18:19:16 +0900

    count

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div>
+      <button @click="increment">Increment</button>
+    </div>
+    <div>{{ count }}</div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
 import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
+@Component({
   components: {
     HelloWorld
   }
+})
+export default class App extends Vue {
+  count = 0
+
+  increment() {
+    this.count++
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
